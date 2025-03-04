c5d596263ab15d821745c8f5d388d4913fc73b46
diego.osorio <diegoso-0989@hotmail.com>
2025-03-04 06:15:44 -0500

    inicio proyecto

---
+++ src/test/java/com/cesde/api/cursos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.api.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
